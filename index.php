b3a23e6eebe9de3420f3f15ab634bb33e795c518
soporte <=>
2020-12-09 19:41:29 -0500

    first commit

---
+++ index.php
@@ -0,0 +1,29 @@
+<?php
+
+
+interface vida{
+    const tipo = array( 'organica', 'no organica'); 
+    public function respirar ($tiempo="5", $cantidad_oxigeno, $estado) ; 
+    public function bombiarfluido(); 
+}
+    interface muerte extends vida{
+        const estado = array( 'pulso leve' , 'no tiene pulso' ); 
+    }
+   
+    class Humano implements muerte{
+        
+        public function saludar(){
+            return "Hola ".muerte::tipo[1]." como estas"; 
+        }
+    
+    public function respirar($tiempo,$cantidad_oxigeno,$estado){
+
+    }
+    public function bombiarfluido(){
+    }
+    public function  pulso(){
+    }
+}
+
+    $obj = new Humano(); 
+    echo $obj->saludar();
